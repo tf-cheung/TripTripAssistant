8fc86df28b600acfa15e6968d69d7d25ff44a102
tf-cheung <tingfung.c@icloud.com>
2023-04-06 18:39:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/tripassistant/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tripassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
