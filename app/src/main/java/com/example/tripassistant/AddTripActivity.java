bce59f6e3ecae6e942670e659291b3a11c44cfbe
tf-cheung <tingfung.c@icloud.com>
2023-04-10 17:02:21 -0700

    add trip activity

---
+++ app/src/main/java/com/example/tripassistant/AddTripActivity.java
@@ -0,0 +1,19 @@
+package com.example.tripassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class AddTripActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip);
+
+
+
+
+    }
+}
