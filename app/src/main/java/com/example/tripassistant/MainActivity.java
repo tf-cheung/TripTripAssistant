2af91b2861dfe3f6741760a024c78b760e5d086e
tim deleo <tdeleo11@gmail.com>
2023-04-18 20:59:25 -0400

    side bar

--- app/src/main/java/com/example/tripassistant/MainActivity.java
+++ app/src/main/java/com/example/tripassistant/MainActivity.java
@@ -3,19 +3,28 @@ package com.example.tripassistant;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private Button signOutButton;
     private TextView userEmailTextView;
+    private LinearLayout sidebarLayout;
+    private ImageButton hamburgerButton;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         userEmailTextView = findViewById(R.id.user_email_textview);
-        signOutButton = findViewById(R.id.signout_button);
+        ImageView signOutButton = findViewById(R.id.signout_button);
+        ImageView settingsButton = findViewById(R.id.settings_button);
+        hamburgerButton = findViewById(R.id.hamburger_button);
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         // Check if the user is logged in
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -39,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         // Set the user email text
         String userEmail = currentUser.getEmail();
         if (userEmail != null) {
+            TextView userEmailTextView = findViewById(R.id.user_email_textview);
             userEmailTextView.setText(userEmail);
         } else {
             userEmailTextView.setText(getString(R.string.email_not_available));
@@ -50,9 +63,16 @@ public class MainActivity extends AppCompatActivity {
                 signOut();
             }
         });
+
+        hamburgerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
+        sidebarLayout = findViewById(R.id.sidebar_layout);
     }
-
-
 
     private void signOut() {
         mAuth.signOut();
@@ -64,5 +84,27 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_sidebar_toggle) {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
